7602ef91d51d7a21d060393ced56344f639410a0
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2022-12-18 16:07:00 +0200

    Readability optimizer for WorldServer.java

---
+++ src/main/java/dev/efnilite/worldserver/Registrator.java
@@ -0,0 +1,30 @@
+package dev.efnilite.worldserver.commands;
+
+import dev.efnilite.worldserver.util.Util;
+import dev.efnilite.worldserver.config.ConfigValue;
+import dev.efnilite.worldserver.WorldServer;
+
+public class Registrator {
+    public void registerAllCommands(WorldServer worldServer) {
+        worldServer.registerCommand("worldserver", new WorldServerCommand());
+        if (ConfigValue.ECONOMY_OVERRIDE_BALANCE_COMMAND) {
+            Util.registerToMap("bal", new BalCommand());
+            Util.registerToMap("balance", new BalCommand());
+        }
+        if (ConfigValue.ECONOMY_OVERRIDE_PAY_COMMAND) {
+            Util.registerToMap("pay", new PayCommand());
+            Util.registerToMap("transfer", new PayCommand());
+        }
+        if (ConfigValue.ECONOMY_OVERRIDE_BALTOP_COMMAND) {
+            Util.registerToMap("baltop", new BaltopCommand());
+            Util.registerToMap("balancetop", new BaltopCommand());
+        }
+    }
+    
+    public void registerAllListeners(WorldServer worldServer) {
+        worldServer.registerListener(new GeneralHandler());
+        worldServer.registerListener(new WorldChatListener());
+        worldServer.registerListener(new WorldTabListener());
+        worldServer.registerListener(new WorldEconomyListener());
+    }
+}
